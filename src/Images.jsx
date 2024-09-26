e68c34481600e0c494fe75d59f72396e61ae169c
Ganesh <ganeshmk247@gmail.com>
2024-09-26 13:26:11 +0530

    initial covertion of html and css

---
+++ src/Images.jsx
@@ -0,0 +1,74 @@
+import myAvatar from "./assets/images/my-avatar.png";
+import logoIco from "./assets/images/logo.ico";
+
+import avatar1 from "./assets/images/avatar-1.png";
+import avatar2 from "./assets/images/avatar-2.png";
+import avatar3 from "./assets/images/avatar-3.png";
+import avatar4 from "./assets/images/avatar-4.png";
+
+import iconApp from "./assets/images/icon-app.svg";
+import iconDesign from "./assets/images/icon-design.svg";
+import iconDev from "./assets/images/icon-dev.svg";
+import iconPhoto from "./assets/images/icon-photo.svg";
+import iconQuote from "./assets/images/icon-quote.svg";
+import logo from "./assets/images/logo.svg";
+
+import logo1 from "./assets/images/logo-1-color.png";
+import logo2 from "./assets/images/logo-2-color.png";
+import logo3 from "./assets/images/logo-3-color.png";
+import logo4 from "./assets/images/logo-4-color.png";
+import logo5 from "./assets/images/logo-5-color.png";
+import logo6 from "./assets/images/logo-6-color.png";
+
+import blog1 from "./assets/images/blog-1.jpg";
+import blog2 from "./assets/images/blog-2.jpg";
+import blog3 from "./assets/images/blog-3.jpg";
+import blog4 from "./assets/images/blog-4.jpg";
+import blog5 from "./assets/images/blog-5.jpg";
+import blog6 from "./assets/images/blog-6.jpg";
+
+import project1 from "./assets/images/project-1.jpg";
+import project2 from "./assets/images/project-2.png";
+import project3 from "./assets/images/project-3.jpg";
+import project4 from "./assets/images/project-4.png";
+import project5 from "./assets/images/project-5.png";
+import project6 from "./assets/images/project-6.png";
+import project7 from "./assets/images/project-7.png";
+import project8 from "./assets/images/project-8.jpg";
+import project9 from "./assets/images/project-9.png";
+
+export const images = {
+  myAvatar,
+  logoIco,
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  iconApp,
+  iconDesign,
+  iconDev,
+  iconPhoto,
+  iconQuote,
+  logo,
+  logo1,
+  logo2,
+  logo3,
+  logo4,
+  logo5,
+  logo6,
+  blog1,
+  blog2,
+  blog3,
+  blog4,
+  blog5,
+  blog6,
+  project1,
+  project2,
+  project3,
+  project4,
+  project5,
+  project6,
+  project7,
+  project8,
+  project9,
+};
